cb06cacf894dca18b84c094bb29d97e19cc5fa77
Pritesh <4444pritesh@gmail.com>
2018-02-11 09:46:27 +0530

    learning purpose

---
+++ src/test/java/com/qa/crm/testcases/HomepageTest.java
@@ -0,0 +1,23 @@
+package com.qa.crm.testcases;
+
+import org.testng.annotations.Test;
+
+public class HomepageTest {
+	
+	@Test
+	public void test1() {
+		System.out.println("public void test1()");
+	}
+	
+	@Test
+	public void test2() {
+
+		System.out.println("public void test2()");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("public void test3()");		
+	}
+
+}
